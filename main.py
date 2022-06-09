ac2f5bd349608d3827d0e42d8136c02d50e1040e
Elis Kostromina <liza.kostr.2005@gmail.com>
2022-06-09 19:11:26 +0700

    visualisation with matplot lib

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ import csv
 import os
 import networkx as nx
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
+
+from matplotlib.figure import Figure
+from matplotlib.collections import PathCollection, LineCollection
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+from grave import plot_network
+from grave.style import use_attributes
 
 from ui.main_window import Ui_MainWindow
 from ui.node_parametizer import Ui_NodeForm
 from ui.edge_parametizer import Ui_EdgeForm
 
 from graph_gen import generate
-from lizkins import draw_graph
+from graph_viz import draw_graph
 
 
 GREEN = (32, 162, 156)
@@ -25,6 +33,9 @@ MAIN_FONT.setWeight(QFont.Bold)
 TITLE_FONT = QFont()
 TITLE_FONT.setWeight(QFont.DemiBold)
 STANDART_FONT = QFont()
+
+COLORS = {"normal": {"L": "#55efc4", "CS": "#ffeaa7", "A": "#74b9ff", "R": "#adef52", "E": "#ef8c69"},
+          "picked": {"L": "#52D6AB", "CS": "#D7C38A", "A": "#6299D6", "R": "#8FCC54", "E": "#D57368"}}
 
 
 class NodeParametizer(Ui_NodeForm, QWidget):
@@ -53,41 +64,39 @@ class EdgeParametizer(Ui_EdgeForm, QWidget):
             label.setFont(TITLE_FONT)
 
 
-
-
 class GraphGUI(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
         self.setFonts()
-        self.setParametizers()
-        self.setCanvas()
+        self.setParametizersAndCanvas()
         self.connect_buttons()
 
-
-        self.cur_config = None    # current config.yaml file, None when no file selected   поле под вопросом
+        self.cur_config = None  # current config.yaml file, None when no file selected   поле под вопросом
+        self.cur_graphml = None
+        self.prev_ind = None
         # можно через generate baseline создавать временный файл, хранящий все параметры конфига до его сохранения
         # возможно нужно сделать файлик по умолчанию в котором можно хранить настройки и после сохранить, типо новый создать ???
 
         self.cur_params = 0 # 0 if none selected, 1 if node selected, 2 if edge selected
 
-
-    def setParametizers(self):
+    def setParametizersAndCanvas(self):
         self.emptyParametizer_label = QLabel("Nothing selected")
         self.emptyParametizer_label.setFont(TITLE_FONT)
         self.emptyParametizer_label.setMargin(25)
         self.emptyParametizer_label.setAlignment(Qt.AlignVCenter|Qt.AlignLeft)
         self.gridLayout.addWidget(self.emptyParametizer_label, 7, 8, 1, 2)
 
-    def setCanvas(self):
+        self.nodeBlock = NodeParametizer()
+        self.edgeBlock = EdgeParametizer()
+
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
         self.gridLayout.addWidget(self.canvas, 3, 0, 6, 7)
 
-    def plot(self, data):
-        self.figure.clf()
-        draw_graph(data)
+        #self.toolbar = NavigationToolbar(self.canvas, self)
+        #self.gridLayout.addWidget(self.toolbar, 9, 0, 1, 5)
 
     def setFonts(self):
         """Sets special font for each label"""
@@ -172,6 +181,7 @@ class GraphGUI(QMainWindow, Ui_MainWindow):
         filename, filter = QFileDialog.getSaveFileName(
             parent=self, caption='Save config file',
             dir='.', filter='*.yaml *.yml')
+        self.cur_config = filename
         with open(filename, 'w', encoding='utf8') as f:
             yaml.dump(self.get_config_data(), f, sort_keys=False)
         self.statusBar.showMessage(f"{self.cur_config} was saved", 5000)
@@ -195,21 +205,139 @@ class GraphGUI(QMainWindow, Ui_MainWindow):
         """Makes xml and displays it to canvas"""
         if self.cur_config is None:
             self.load_config()
-        graph, db_predata = generate(self.get_config_data()) # [['id', 'cont_id', 'empty', 'class_type', 'floor', 'rack', 'cell', 'side'], ['1_A_1_1_1_L', None, True, 'A', 1, 1, 1, 'L'],...]
-        self.plot(db_predata[1:])
+        graph, db_predata, graph_path, db_predata_path = generate(self.get_config_data()) # [['id', 'cont_id', 'empty', 'class_type', 'floor', 'rack', 'cell', 'side'], ['1_A_1_1_1_L', None, True, 'A', 1, 1, 1, 'L'],...]
+        self.cur_graphml = graph_path
+        cur_floor = self.curFloor_spinBox.value()
+        #draw_graph(cur_floor, self.cur_config, self.cur_graphml)
+        self.vis_graph(cur_floor, graph)
 
         self.statusBar.showMessage(f"temp files (xml and csv) were created", 5000)
 
+    def vis_graph(self, cur_floor, G):
+
+        self.figure.clf()
+
+        with open(self.cur_config) as f:
+            storage = yaml.safe_load(f)
+
+        modules = storage["modules"]
+
+        pos = {}
+        nodes = G.nodes()
+
+        base_gap_x = 20
+        base_gap_y = 30
+        scale = 10
+
+        self.COLOR_MAP = []
+        cur_nodes = []
+
+        for node in nodes:
+            module_id, typo, floor, rack, cell = map(lambda i: int(i) if i.isdigit() else i, node.split("_"))
+            if floor == cur_floor or typo == "L":
+                cur_nodes.append(node)
+
+        cur_floor_G = G.subgraph(cur_nodes).to_undirected()
+
+        for node in cur_floor_G.nodes():
+            module_id, typo, floor, rack, cell = map(lambda i: int(i) if i.isdigit() else i, node.split("_"))
+            if typo == "L":
+                pos[node] = [
+                    # x
+                    base_gap_x * max(modules[module_id - 1]["cross"]["count"],
+                                     len(modules[module_id - 1]["rack"]["cs_link"])) // len(
+                        modules[module_id - 1]["lift"]["cs_link"]) + base_gap_x * rack,
+                    # y
+                    -base_gap_y * scale * modules[module_id - 1]["lift"]["weight"]
+                ]
+
+            elif typo == "CS":
+                pos[node] = [
+                    base_gap_x * max(modules[module_id - 1]["cross"]["count"],
+                                     len(modules[module_id - 1]["rack"]["cs_link"])) // len(
+                        modules[module_id - 1]["lift"]["cs_link"]) + base_gap_x * rack,
+                    10
+                ]
+
+            elif typo == "A":
+                pos[node] = [
+                    base_gap_x * max(modules[module_id - 1]["cross"]["count"],
+                                     len(modules[module_id - 1]["rack"]["cs_link"])) // len(
+                        modules[module_id - 1]["lift"]["cs_link"]) + base_gap_x * rack,
+                    base_gap_y * scale * cell
+                ]
+            elif typo == "R":
+                pos[node] = [
+                    # x
+                    base_gap_x * max(modules[module_id - 1]["cross"]["count"],
+                                     len(modules[module_id - 1]["rack"]["cs_link"])) // len(
+                        modules[module_id - 1]["lift"]["cs_link"]) + base_gap_x * rack,
+                    # y
+                    -base_gap_y * scale * modules[module_id - 1]["lift"]["weight"]
+                ]
+
+            elif typo == "E":
+                pos[node] = [
+                    # x
+                    base_gap_x * max(modules[module_id - 1]["cross"]["count"],
+                                     len(modules[module_id - 1]["rack"]["cs_link"])) // len(
+                        modules[module_id - 1]["lift"]["cs_link"]) + base_gap_x * rack,
+                    # y
+                    -base_gap_y * scale * modules[module_id - 1]["lift"]["weight"]
+                ]
+            self.COLOR_MAP.append(COLORS["normal"][typo])
+
+        #draw graph
+        #plt.gca().set_axis_off()
+        nodes = nx.draw_networkx_nodes(cur_floor_G, pos, node_color=self.COLOR_MAP, label=1, node_size=100)
+
+        nodes.set_picker(5)
+
+        nx.draw_networkx_labels(cur_floor_G, pos, font_size=5)
+        edges = nx.draw_networkx_edges(cur_floor_G, pos, width=1)
+
+        edges.set_picker(5)
+
+        nx.draw_networkx_edge_labels(cur_floor_G, pos, edge_labels=nx.get_edge_attributes(cur_floor_G, 'weight'), font_size=5)
+        #artist = plot_network(cur_floor_G, pos=pos, node_style=use_attributes(), edge_style=use_attributes())
+        print(artist)
+        artist.set_picker(10)
+
+
+        self.pos = pos
+        # Bind our onpick() function to pick events:
+        self.canvas.mpl_connect('pick_event', self.onpick)
+        self.canvas.draw_idle()
+
+    def onpick(self, event):
+        if isinstance(event.artist, PathCollection):
+            all_nodes = event.artist
+            ind = event.ind[0]  # event.ind is a single element array.
+            nodes = list(self.pos.keys())
+            this_node_name = nodes[ind]
+
+            # Set the colours for all the nodes, highlighting the picked node with
+            # a different colour:
+
+            self.COLOR_MAP[ind]=COLORS['picked'][this_node_name.split("_")[1]]
+            if self.prev_ind is not None:
+                self.COLOR_MAP[self.prev_ind] = COLORS["normal"][nodes[self.prev_ind].split("_")[1]]
+
+            all_nodes.set_facecolors(self.COLOR_MAP)
+            # Update the plot to show the change:
+            self.canvas.draw_idle()
+            self.prev_ind = ind
+        elif isinstance(event.artist, LineCollection):
+            pass
+
     def nodeChosedEvent(self):
         """Displays Node Parametizer Widget"""
-        self.nodeBlock = NodeParametizer()
-        self.nodeBlock.setupUi(self)
+        self.gridLayout.removeItem(self.gridLayout.itemAtPosition(7, 8))
         self.gridLayout.addLayout(self.nodeBlock.gridLayout, 7, 8, 1, 2)
 
     def edgeChosedEvent(self):
         """Displays Edge Parametizer Widget"""
-        self.edgeBlock = EdgeParametizer()
-        self.edgeBlock.setupUi(self)
+        self.gridLayout.removeItem(self.gridLayout.itemAtPosition(7, 8))
         self.gridLayout.addLayout(self.edgeBlock.gridLayout, 7, 8, 1, 2)
 
     def closeEvent(self, event):
